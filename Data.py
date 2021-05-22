c5ef47c8543a140cf4a4348f73a9de11a02317a3
AnonymousGyaan <84640731+AnonymousGyaan@users.noreply.github.com>
2021-05-23 00:16:00 +0530

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,5 @@
+lst = [
+    ("Meeting_ID", "Starting_time", "Ending_time"), ]
+
+
+# if you want to add more link just copy this ("Meeting_ID", "Starting_time", "Ending_time") and paste after comma(,)
